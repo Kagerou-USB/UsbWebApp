c388034d5d5e4e5d86c29e0cbb8ac0813617d134
Kagerou USB <f12c052jt08e163c@gmail.com>
2020-08-27 21:59:18 +0900

    first commit

---
+++ src/main/java/com/usb/usbwebapp/UsbWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.usb.usbwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsbWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsbWebAppApplication.class, args);
+	}
+
+}
