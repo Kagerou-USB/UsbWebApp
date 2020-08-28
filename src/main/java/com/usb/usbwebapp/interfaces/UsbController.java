b9b07ae4a50bdedfa4b2f5e09ed2c63bce8b59b1
Kagerou USB <f12c052jt08e163c@gmail.com>
2020-08-28 15:48:42 +0900

    USB Pageを追加

--- src/main/java/com/usb/usbwebapp/interfaces/UsbController.java
+++ src/main/java/com/usb/usbwebapp/interfaces/UsbController.java
@@ -1,13 +1,39 @@
 package com.usb.usbwebapp.interfaces;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class UsbController {
 	
-	@RequestMapping(value = "/")
-	public String index() {
-		return "こんにちは、USB!!!";
+	@RequestMapping("/")
+	public ModelAndView index(ModelAndView mav) {
+		mav.addObject("res1", "USB text1");
+		mav.addObject("res2", "USB text2");
+		mav.addObject("res3", "USB text3");
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	@RequestMapping("/sum/{num1}/{num2}")
+	public ModelAndView sum(
+			@PathVariable int num1,
+			@PathVariable int num2,
+			ModelAndView mav) {
+		mav.addObject("res1", (num1 + num2));
+		mav.setViewName("index");
+		return mav;
+	}
+	
+	@RequestMapping("/diff/{num1}/{num2}")
+	public ModelAndView diff(
+			@PathVariable int num1,
+			@PathVariable int num2,
+			ModelAndView mav) {
+		mav.addObject("res1", (num1 - num2));
+		mav.setViewName("index");
+		return mav;
 	}
 }
